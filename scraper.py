4503cd6046a426780978690c3a1f7daa4c74c78c
Brett Lykins <lykinsbd@gmail.com>
2021-02-24 11:40:59 -0600

    add debug Slack message

--- scraper.py
+++ scraper.py
@@ -1,9 +1,12 @@
-import requests
-import yaml
-import slack
-import time
+
 import logging
 import os
+import time
+from typing import Any, Dict, List
+import yaml
+
+import requests
+import slack
 
 with open('config.yml', 'r') as f:
     CONFIG = yaml.load(f, Loader=yaml.FullLoader)
@@ -17,72 +20,102 @@ APPTS_FOUND = {}
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 log = logging.getLogger('covid_appt_bot')
+log.setLevel(level=os.environ.get("LOGLEVEL", "INFO"))
 
 def post_to_slack(message, city):
     client = slack.WebClient(token=TOKEN)
     client.chat_postMessage(
         channel=CHANNEL,
         blocks=message,
-        as_user=False,
+        # as_user=False,  # Throws SlackApiError, Deprecated
         username=city,
         icon_emoji=ICON
     )
 
-def format_block(payload):
-    response = [
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "<!here> An appointment was located at your local *H-E-B*!"
-            },
-            "fields": [
-                {
+def format_block(payload: Dict[str, str], success: bool) -> List[Dict[str, Any]]:
+    """
+    Given the response payload for a location, format it for slack.
+
+    If there was no success, format a failure message.
+    """
+
+    if success:
+        response = [
+            {
+                "type": "section",
+                "text": {
                     "type": "mrkdwn",
-                    "text": "*Type*"
+                    "text": "<!here> An appointment was located at your local *H-E-B*!"
                 },
-                {
-                    "type": "plain_text",
-                    "text": payload['type']
-                },
-                {
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Type*"
+                    },
+                    {
+                        "type": "plain_text",
+                        "text": payload['type']
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Location*"
+                    },
+                    {
+                        "type": "plain_text",
+                        "text": payload['name']
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Available Time Slots*"
+                    },
+                    {
+                        "type": "plain_text",
+                        "text": str(payload['openTimeslots'])
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Available Appointment Slots*"
+                    },
+                    {
+                        "type": "plain_text",
+                        "text": str(payload['openAppointmentSlots'])
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Address*"
+                    },
+                    {
+                        "type": "plain_text",
+                        "text": f"{payload['street']}\n{payload['city']},{payload['state']} {payload['zip']}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Link*"
+                    },
+                    {
+                        "type": "plain_text",
+                        "text": "https://vaccine.heb.com/scheduler"
+                    }
+                ]
+            }
+        ]
+    else:
+        response = [
+            {
+                "type": "section",
+                "text": {
                     "type": "mrkdwn",
-                    "text": "*Location*"
+                    "text": "An appointment was NOT located at any of your local *H-E-B* locations..."
                 },
-                {
-                    "type": "plain_text",
-                    "text": payload['name']
-                },
-                {
-                    "type": "mrkdwn",
-                    "text": "*Available Time Slots*"
-                },
-                {
-                    "type": "plain_text",
-                    "text": str(payload['openTimeslots'])
-                },
-                {
-                    "type": "mrkdwn",
-                    "text": "*Available Appointment Slots*"
-                },
-                {
-                    "type": "plain_text",
-                    "text": str(payload['openAppointmentSlots'])
-                },
-                {
-                    "type": "mrkdwn",
-                    "text": "*Address*"
-                },
-                {
-                    "type": "plain_text",
-                    "text": f"{payload['street']}\n{payload['city']},{payload['state']} {payload['zip']}"
-                }
-            ]
-        }
-    ]
+            }
+        ]
     return response
 
 while True:
+
+    # Figure out if we found _any_ appointments
+    any_appts_found = False
+
     log.info("Querying API")
     r = requests.get(URL)
     json = r.json()
@@ -92,11 +125,15 @@ while True:
                 log.info(f'Appointment found at {i["name"]}')
                 if not APPTS_FOUND.get(i['name'], False):
                     log.info('Sending message to slack')
-                    post_to_slack(format_block(i), i.get('city'))
+                    post_to_slack(format_block(i, True), i.get('city'))
                     APPTS_FOUND[i['name']] = True
+                    any_appts_found = True
                 else:
                     log.info('Message previously sent to slack')
             else:
                 log.debug('No appointments found.')
                 APPTS_FOUND[i['name']] = False
+
+    if log.level == 10 and not any_appts_found:
+        post_to_slack(format_block(i, False), i.get('city'))
     time.sleep(10)
